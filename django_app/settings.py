0b49c003202a329bee385cb8eaeefa2985f63cb0
Dhruva Dave <dhruvadave5297@gmail.com>
2020-09-06 17:55:13 +0530

    add Procfile

--- django_app/settings.py
+++ django_app/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/static/'
 
 # sent password reset URL in console 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
